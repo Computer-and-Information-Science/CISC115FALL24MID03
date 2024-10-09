3bcf415635ff20a8475a40002d1912a5b1407974
frank8sanchez7 <frank.sanchez@student.northampton.edu>
2024-10-09 22:15:41 +0000

    in class assignment

--- Inclass Assignment/inclassassignment_sanchez.cpp
+++ Inclass Assignment/inclassassignment_sanchez.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
+int main() {
+double netbalance, interest, averagedailybalance;
+int d1, d2, payment;
+
+
+cin >> d1;
+cin >> d2;
+cin >> netbalance; 
+cin >> payment;
+
+averagedailybalance = (netbalance * d1 - payment * d2) / d1; 
+interest = averagedailybalance * 0.0152;
+
+cout << fixed << setprecision(2) << "Interest: " <<interest;
+
+return 0;
+
+
+}
